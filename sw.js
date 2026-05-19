696693006be03de5e9eb7e48beb5446f018e98cf
Jaymombu <168933839+Jaymombu@users.noreply.github.com>
2026-05-19 22:03:18 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "social-app-v1";
+const CACHE_NAME = "social-app-v2";
 
 const urlsToCache = [
   "./",
@@ -8,18 +8,79 @@ const urlsToCache = [
   "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+// INSTALL
+self.addEventListener("install", (event) => {
+
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+// ACTIVATE
+self.addEventListener("activate", (event) => {
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+
+      return Promise.all(
+        cacheNames.map((cache) => {
+
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+
+        })
+      );
+
     })
   );
+
+  self.clients.claim();
+
+});
+
+// FETCH
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(
+
+    caches.match(event.request).then((cachedResponse) => {
+
+      // RETURN CACHE
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // FETCH NETWORK
+      return fetch(event.request)
+        .then((networkResponse) => {
+
+          // CLONE RESPONSE
+          const clonedResponse = networkResponse.clone();
+
+          // SAVE NEW FILES
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, clonedResponse);
+          });
+
+          return networkResponse;
+
+        })
+        .catch(() => {
+
+          // OFFLINE FALLBACK
+          if (event.request.mode === "navigate") {
+            return caches.match("./index.html");
+          }
+
+        });
+
+    })
+
+  );
+
 });
